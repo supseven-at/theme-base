d896f704cbae791ccba7f962d6513d954bf385db
Georg Großberger <g.grossberger@supseven.at>
2024-12-11 12:31:02 +0100

    [TASK] Do not load TSconfig or RTE setups in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,24 +2,7 @@
 
 declare(strict_types=1);
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors']['DeferredBackendImageProcessor']);
 
-(static function ($extKey): void {
-    ExtensionManagementUtility::addUserTSConfig('@import \'EXT:' . $extKey . '/Configuration/user.tsconfig\'');
-    ExtensionManagementUtility::addTypoScriptSetup('@import \'EXT:' . $extKey . '/Configuration/TypoScript/root.typoscript\'');
-
-    // Register own RTE (ckeditor) presets
-    $rtePresets = [
-        'default' => 'Default',
-    ];
-    foreach ($rtePresets as $identifier => $fileName) {
-        $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['theme_' . $identifier]
-            = 'EXT:' . $extKey . '/Configuration/RTE/' . $fileName . '.yaml';
-    }
-
-    unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors']['DeferredBackendImageProcessor']);
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['initializeFormElement'][1713266835]
-        = \Supseven\ThemeBase\Hooks\PrefillFormFieldsWithTestValues::class;
-
-})('theme_base');
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['initializeFormElement'][1713266835]
+    = \Supseven\ThemeBase\Hooks\PrefillFormFieldsWithTestValues::class;
